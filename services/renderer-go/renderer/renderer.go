b6e88b17dd4ee04a66a4bef54868703240466107
teruya <teru0x01.sheep@gmail.com>
2020-08-25 14:24:04 +0900

    mainで正規表現の初期化を行うよう変更

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -1,25 +1,12 @@
 package renderer
 
 import (
-	"bytes"
 	"context"
-	"html/template"
-	"regexp"
+
+	"github.com/hatena/Hatena-Intern-2020/services/renderer-go/converter"
 )
 
-var urlRE = regexp.MustCompile(`https?://[^\s]+`)
-var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
-
 // Render は受け取った文書を HTML に変換する
-func Render(ctx context.Context, src string) (string, error) {
-	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
-	})
-	return html, nil
+func Render(ctx context.Context, src string, lcs []converter.LineConverter, wcs []converter.WholeConverter) (string, error) {
+	return converter.Execute(src, lcs, wcs)
 }
