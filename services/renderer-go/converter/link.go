0426a4252ca25eac79e23da67b5baf1280d24a06
teruya <teru0x01.sheep@gmail.com>
2020-08-25 12:50:23 +0900

    link記法を実装

--- services/renderer-go/converter/link.go
+++ services/renderer-go/converter/link.go
@@ -1,3 +1,22 @@
 package converter
 
-type LinkConverter struct{}
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type LinkConverter struct {
+	pattern *regexp.Regexp
+}
+
+func (lc *LinkConverter) convert(line string) (string, error) {
+	matches := lc.pattern.FindAllStringSubmatch(line, -1)
+	if len(matches) == 0 {
+		return line, nil
+	}
+	for _, m := range matches {
+		line = strings.Replace(line, m[0], fmt.Sprintf("<a href=%s>%s</a>", m[2], m[1]), 1)
+	}
+	return line, nil
+}
---
+++ services/renderer-go/converter/link_test.go
@@ -0,0 +1,43 @@
+package converter
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TC struct {
+	in  string
+	out string
+}
+
+func TestLink(t *testing.T) {
+	testCases := []TC{
+		TC{
+			in:  "[hoge](http://google.com)",
+			out: "<a href=http://google.com>hoge</a>",
+		},
+		TC{
+			in:  "[ほげ](http://google.com)",
+			out: "<a href=http://google.com>ほげ</a>",
+		},
+		TC{
+			in:  "[hoge](http://お名前.com)",
+			out: "<a href=http://お名前.com>hoge</a>",
+		},
+		TC{
+			in:  "1つ目は[hoge](http://google.com)で，2つ目は[hoge](http://google.com)です．",
+			out: "1つ目は<a href=http://google.com>hoge</a>で，2つ目は<a href=http://google.com>hoge</a>です．",
+		},
+	}
+
+	lc := &LinkConverter{
+		pattern: regexp.MustCompile(`\[(.[^\]]*)\]\((https?://.[^\)]*)\)`),
+	}
+	for _, testCase := range testCases {
+		result, err := lc.convert(testCase.in)
+		assert.NoError(t, err)
+		assert.Equal(t, testCase.out, result)
+	}
+}
